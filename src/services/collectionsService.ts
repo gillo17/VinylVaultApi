d77097592f51a91a10e572cfc4296edcf39b4a6b
Gillon Paterson <gillonpaterson@MacBook-Pro.local>
2025-03-03 13:37:31 +0000

    Remove added vinyl from wishlist

--- src/services/collectionsService.ts
+++ src/services/collectionsService.ts
@@ -6,6 +6,7 @@ import { CollectionsMapper } from "../mappers/collectionsMapper";
 import { SpotifyService } from "./spotifyService";
 import { SpotifyMapper } from "../mappers/spotifyMapper";
 import { spotifyAlbumData } from "../models/spotify";
+import { WishlistDao } from "../daos/wishlistDao";
 
 export class CollectionsService {
 
@@ -13,7 +14,8 @@ export class CollectionsService {
         @inject(Types.CollectionsDao) private collectionDao: CollectionsDao,
         @inject(Types.CollectionsMapper) private collectionMapper: CollectionsMapper,
         @inject(Types.SpotifyService) private spotifyService: SpotifyService,
-        @inject(Types.SpotifyMapper) private spotifyMapper: SpotifyMapper
+        @inject(Types.SpotifyMapper) private spotifyMapper: SpotifyMapper,
+        @inject(Types.WishlistDao) private wishlistDao: WishlistDao
     ) {}
 
     public async createCollection(collection: CollectionModel) {
@@ -38,6 +40,12 @@ export class CollectionsService {
         const albumInfo = await this.spotifyService.getAlbumInfo(data.spotifyAlbumID);
 
         const mappedAlbumInfo: spotifyAlbumData = await this.spotifyMapper.mapSpotifyAlbumData(albumInfo);
+
+        const existsInWishlist = await this.wishlistDao.searchAlbumInWishlist(data.userId, data.spotifyAlbumID);
+
+        if (existsInWishlist) {
+            await this.wishlistDao.removeAlbumfromWishlist(data.userId, data.spotifyAlbumID)
+        }
 
         this.collectionDao.saveVinylToCollection(mappedAlbumInfo, data.collectionID);
     }
--- test/unit/services/collectionService.test.ts
+++ test/unit/services/collectionService.test.ts
@@ -1,10 +1,12 @@
 import { CollectionsDao } from '../../../src/daos/collectionsDao';
-import Collection from '../../../src/models/collections';
+import Collection, { saveVinylToCollectionModel } from '../../../src/models/collections';
 import { CollectionsService } from '../../../src/services/collectionsService';
 import { describe, expect, test, jest } from '@jest/globals';
 import { CollectionsMapper } from '../../../src/mappers/collectionsMapper';
 import { SpotifyMapper } from '../../../src/mappers/spotifyMapper';
 import { SpotifyService } from '../../../src/services/spotifyService';
+import { WishlistDao } from '../../../src/daos/wishlistDao';
+import { mock } from 'node:test';
 
 
 describe('Collection Service Tests', () => {
@@ -13,7 +15,8 @@ describe('Collection Service Tests', () => {
     const collectionMapper = new CollectionsMapper();
     const spotifyMapper= new SpotifyMapper();
     const spotifyService = new SpotifyService();
-    const collectionsService = new CollectionsService(collectionsDao, collectionMapper, spotifyService, spotifyMapper);
+    const wishlistDao = new WishlistDao();
+    const collectionsService = new CollectionsService(collectionsDao, collectionMapper, spotifyService, spotifyMapper, wishlistDao);
 
     const testCollection = new Collection({
         _id: 'testId',
@@ -23,6 +26,13 @@ describe('Collection Service Tests', () => {
         userID: 'testUserID'
     });
 
+    const mockAlbumData = {
+        name: 'testName',
+        artists: [{ name: 'testArtist' }],
+        id: 'testID',
+        images: [{ url: 'testURL' }]
+    }
+
     test('createCollection is called when a collection is created', async () => {
         const createCollectionSpy = jest
             .spyOn(collectionsDao, 'createCollection')
@@ -31,6 +41,65 @@ describe('Collection Service Tests', () => {
         collectionsService.createCollection(testCollection);
 
         expect(createCollectionSpy).toHaveBeenCalledWith(testCollection);
+    })
+
+    test("save vinyl to collection to be called when a vinyl is added", async () => {
+        const mockSaveVinylToCollection : saveVinylToCollectionModel = {
+            spotifyAlbumID: 'testID',
+            collectionID: 'testCollectionID',
+            userId: 'testUserID'
+        }
+
+        const saveToCollectionSpy = jest
+            .spyOn(collectionsDao, 'saveVinylToCollection')
+            .mockResolvedValue("saved");
+
+        const checkWishlistSpy = jest
+            .spyOn(wishlistDao, 'searchAlbumInWishlist')
+            .mockResolvedValue(false);
+
+        const getAlbumDataSpy = jest
+            .spyOn(spotifyService, 'getAlbumInfo')
+            .mockResolvedValue(mockAlbumData);
+
+        await collectionsService.saveVinylToCollection(mockSaveVinylToCollection);
+
+        expect(saveToCollectionSpy).toHaveBeenCalled();
+        expect(checkWishlistSpy).toHaveBeenCalledWith(mockSaveVinylToCollection.userId, mockAlbumData.id);
+        expect(getAlbumDataSpy).toHaveBeenCalled();
+    
+    })
+
+        test("remove vinyl from wishlist should be called if vinyl to be added is in the wishlist", async () => {
+        const mockSaveVinylToCollection : saveVinylToCollectionModel = {
+            spotifyAlbumID: 'testID',
+            collectionID: 'testCollectionID',
+            userId: 'testUserID'
+        }
+
+        const saveToCollectionSpy = jest
+            .spyOn(collectionsDao, 'saveVinylToCollection')
+            .mockResolvedValue("saved");
+
+        const checkWishlistSpy = jest
+            .spyOn(wishlistDao, 'searchAlbumInWishlist')
+            .mockResolvedValue(true);
+
+        const removeAlbumfromWishlistSpy = jest
+            .spyOn(wishlistDao, 'removeAlbumfromWishlist')
+            .mockImplementation(() => Promise.resolve(true));
+
+        const getAlbumDataSpy = jest
+            .spyOn(spotifyService, 'getAlbumInfo')
+            .mockResolvedValue(mockAlbumData);
+
+        await collectionsService.saveVinylToCollection(mockSaveVinylToCollection);
+
+        expect(saveToCollectionSpy).toHaveBeenCalled();
+        expect(removeAlbumfromWishlistSpy).toHaveBeenCalled();
+        expect(checkWishlistSpy).toHaveBeenCalledWith(mockSaveVinylToCollection.userId, mockAlbumData.id);
+        expect(getAlbumDataSpy).toHaveBeenCalled();
+    
     })
 
 
